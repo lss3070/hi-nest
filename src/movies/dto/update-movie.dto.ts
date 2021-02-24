cbdc0fc45dfad1ee8d9dca3431a83a7e56cc71d8
songseop.lee <lss3070@hanmail.net>
2021-02-24 23:54:37 +0900

    unit test&&dto update

---
+++ src/movies/dto/update-movie.dto.ts
@@ -0,0 +1,6 @@
+import { IsString, IsNumber } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { fromEventPattern } from 'rxjs';
+import { CreateMovieDto } from './create-movie.dto';
+
+export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
